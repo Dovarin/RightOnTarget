d87b804fbc62559e48aa7fe4962e0ada6d47c432
Данил <ddovarin@gmail.com>
2021-08-23 15:00:11 +0300

    init

---
+++ Frimeworks/Frime.framework/Headers/Frime.h
@@ -0,0 +1,18 @@
+//
+//  Frime.h
+//  Frime
+//
+//  Created by Данил Гусев on 19.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Frime.
+FOUNDATION_EXPORT double FrimeVersionNumber;
+
+//! Project version string for Frime.
+FOUNDATION_EXPORT const unsigned char FrimeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Frime/PublicHeader.h>
+
+
---
+++ Game1.1/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  Right on target
+//
+//  Created by Admin on 05.11.2020.
+//
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    // MARK: - Переходы
+    
+    @IBAction func previousScreen( unwindSegue: UIStoryboardSegue ) {
+        return
+    }
+
+}
